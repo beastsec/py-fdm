e4a6f8ef5a511eeac750b580a71a6400a66ec133
Gabriel Romero <gabrielromero0499@gmail.com>
2021-08-26 21:44:23 -0400

    Add thread_download to handler.py

--- src/py-fdm/core/network/handler.py
+++ src/py-fdm/core/network/handler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
+import threading
 import requests
 
 class Handler:
@@ -14,15 +15,35 @@ class Handler:
         r = requests.head(self.url)
         return(r.headers)
         
-    def simple_download(self):
+    def download_file(self):
         """
         Reserve file size on disk and then download the file and fill that reserved space.
         """
         s = requests.Session()
         with open(self.filename, 'wb') as output_file:
-            with s.get(self.url, stream=True) as getting_file:
+            with s.get(self.url, timeout=30, stream=True) as getting_file:
                 file_size = int(getting_file.headers['content-length'])
                 output_file.write(b'\x00' * file_size)
                 output_file.seek(0, 0)
                 for chunk in getting_file.iter_content(chunk_size=1024):
                     if chunk: output_file.write(chunk)
+    def threads_download(self, max_threads = 4):
+        s = requests.Session()
+        file_size = int(s.head(self.url).headers['content-length'])
+        with open(self.filename, 'wb') as create_dummy:
+            create_dummy.write(b'\x00' * file_size)
+        def _donwload_file(start, end):
+            headers = {'Range': 'bytes=%d-%d' % (start, end)}
+            with open(self.filename, 'r+b') as output_file:
+                with s.get(self.url, headers=headers, timeout=30, stream=True) as getting_file:
+                    output_file.seek(start)
+                    for chunk in getting_file.iter_content(chunk_size=1024):
+                        if chunk: output_file.write(chunk)
+        part = file_size // max_threads
+        threads = []
+        for thread in range(max_threads):
+            start = part * thread
+            end = start + part
+            t = threading.Thread(target=_donwload_file, args=(start, end))
+            threads.append(t)
+            t.start()
