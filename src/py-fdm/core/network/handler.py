6650fbba9e900a1428c54db1bfcadf0c4d7f8c34
Gabriel Romero <gabrielromero0499@gmail.com>
2021-10-16 18:50:26 -0400

    Optimize code

--- src/py-fdm/core/network/handler.py
+++ src/py-fdm/core/network/handler.py
@@ -2,52 +2,44 @@
 #-*- coding: utf-8 -*-
 import threading
 import requests
-import json
 
-class Handler:
-    def __init__(self, url):
-        self.url = url
-        self.filename = url.split("/")[-1]
-
-    def check_host(self):
-        """
-        check the accepted headers.
-        """
-        r = requests.head(self.url)
-        return json.dumps(dict(r.headers))
-        
-    def download_file(self):
-        """
-        Reserve file size on disk and then download the file and fill that reserved space.
-        """
-        s = requests.Session()
-        with open(self.filename, 'wb') as output_file:
-            with s.get(self.url, timeout=30, stream=True) as getting_file:
-                file_size = int(getting_file.headers['content-length'])
-                output_file.write(b'\x00' * file_size)
-                output_file.seek(0, 0)
-                for chunk in getting_file.iter_content(chunk_size=1024):
-                    if chunk: output_file.write(chunk)
-    def threads_download(self, max_threads = 4):
+class Http:
+    def __init__(self):
+        self.headers = {'User-Agent': 'py-fdm/0.1'}
+    def multi_threads():
         """
         Perform a multi-part file download using threads.
         """
+        def levelup(func):
+            def wrapper(self, url, threads, mthreads):
+                if mthreads == False or mthreads <= 0:
+                    func(self, url)
+                else:
+                    chost = requests.head(url)
+                    fsize = int(chost.headers['content-length'])
+                    part = fsize // mthreads
+                    threads = []
+                    for thread in range(mthreads):
+                        sbyte = part * thread
+                        ebyte = sbyte + part
+                        t = threading.Thread(target=func, kwargs={'self': self, 'url': url, 'threads': True, 'mthreads': mthreads, 'start': sbyte, 'end': ebyte})
+                        threads.append(t)
+                        t.start()
+            return wrapper
+        return levelup
+    @multi_threads()
+    def download(self, url, threads = False, mthreads = 0, start = 0, end = 0):
+        """
+        Reserve file size on disk and then download the file and fill that reserved space.
+        """
+        fname = url.split("/")[-1]
+        if threads == True: self.headers['Range'] = f'bytes={start}-{end}'
         s = requests.Session()
-        file_size = int(s.head(self.url).headers['content-length'])
-        with open(self.filename, 'wb') as create_dummy:
-            create_dummy.write(b'\x00' * file_size)
-        def _donwload_file(start, end):
-            headers = {'Range': 'bytes=%d-%d' % (start, end)}
-            with open(self.filename, 'r+b') as output_file:
-                with s.get(self.url, headers=headers, timeout=30, stream=True) as getting_file:
-                    output_file.seek(start)
-                    for chunk in getting_file.iter_content(chunk_size=1024):
-                        if chunk: output_file.write(chunk)
-        part = file_size // max_threads
-        threads = []
-        for thread in range(max_threads):
-            start = part * thread
-            end = start + part
-            t = threading.Thread(target=_donwload_file, args=(start, end))
-            threads.append(t)
-            t.start()
+        with open(fname, 'w+b') as ofile:
+            with s.get(url, headers=self.headers, timeout=30, stream=True) as gfile:
+                fsize = int(gfile.headers['content-length'])
+                ofile.write(b'\x00' * fsize)
+                ofile.seek(start)
+                for chunk in gfile.iter_content(chunk_size=1024):
+                    if chunk: ofile.write(chunk)
+  
