537ae15c9b131054bbb805aedd66570fe50caf7d
N0ob\x20Code <deibygerez95@gmail.com>
2021-10-17 15:09:18 -0400

    Initial commit

---
+++ web/js/js.js
@@ -0,0 +1,24 @@
+
+function CredsVerify(){
+    const usernme = document.getElementById('username');
+    const password = document.getElementById('password');
+    const user_error = document.getElementById('error-user');
+    const pass_error = document.getElementById('error-password');
+    if (!username.value){
+        username.className = "form-control is-invalid";
+        user_error.style.display = 'inline';
+    }else{
+        username.className = "form-control";
+        user_error.style.display = 'none'
+    }
+    if (!password.value){
+        password.className = "form-control is-invalid";
+        pass_error.style.display = 'inline';
+    }else{
+        password.className = "form-control";
+        pass_error.style.display = 'none';
+    }
+}
+
+const login = document.getElementById('login');
+login.addEventListener('click',CredsVerify);
