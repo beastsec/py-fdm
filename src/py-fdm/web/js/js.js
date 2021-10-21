55cb66b3223ca2ed5ded821613c885d94f2f85ec
N0ob\x20Code <deibygerez95@gmail.com>
2021-10-20 22:43:53 -0400

    Empty login message show from js

--- src/py-fdm/web/js/js.js
+++ src/py-fdm/web/js/js.js
@@ -7,17 +7,19 @@ function CredsVerify(){
     const user_error = document.getElementById('error-user');
     const pass_error = document.getElementById('error-password');
     if (!username.value){
-        username.className = "form-control is-invalid";
+        username.className = 'form-control is-invalid';
         user_error.style.display = 'inline';
+        user_error.textContent = 'Username is required';
     }else{
-        username.className = "form-control";
+        username.className = 'form-control';
         user_error.style.display = 'none'
     }
     if (!password.value){
-        password.className = "form-control is-invalid";
+        password.className = 'form-control is-invalid';
         pass_error.style.display = 'inline';
+        pass_error.textContent = 'Password is required';
     }else{
-        password.className = "form-control";
+        password.className = 'form-control';
         pass_error.style.display = 'none';
     }
 }
